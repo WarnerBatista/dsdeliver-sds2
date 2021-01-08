ea1942e742856a8689b2e71b807c83e8697eaaae
warner <warnerbatista@gmail.com>
2021-01-08 16:39:42 -0300

    List items

---
+++ front-web/src/Orders/ProductsList.tsx
@@ -0,0 +1,19 @@
+import ProductCard from "./ProductCard";
+import { Product } from "./Types";
+type props={
+    products: Product[];
+}
+
+function ProductsList({products}:props){
+    
+    return(
+        <div className="orders-list-container">
+            <div className="orders-list-items">
+                {products.map(product =>(
+                    <ProductCard key={product.id} product={product} />
+                ))}
+            </div>
+        </div>
+    )
+}
+export default ProductsList;
